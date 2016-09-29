f119c44347adec531bf252f99b332e3c7bbc4d89
Toni Pikkarainen <toni.t.pikkarainen@gmail.com>
2016-09-29 14:21:13 +0300

    oma javascripti joka päivittää navlistaa

---
+++ oma.js
@@ -0,0 +1,26 @@
+/* Automaatisoidaan navigointilistan päivitykset, kun tehdään uusia sivuja
+Eli täytyy muuttaa vain silmukan kokoa*/
+
+window.onload=function(){
+	var lista = document.getElementById("dropLista");
+	
+	var kotiLi = document.createElement("li");
+	var kotiA = document.createElement("a");
+	kotiA.setAttribute("href","index.html");
+	kotiLi.appendChild(kotiA);
+	
+	lista.appendChild(kotiLi);
+	//kotiA.setAttribute("value","Koti");
+	kotiA.textContent = "Koti";
+	for(var i=0 ; i<5 ; i++){
+		var luku = i+1;
+		var listaLi = document.createElement("li");
+		var listaA = document.createElement("a");
+		listaA.setAttribute("href","teht"+luku+".html");
+		listaLi.appendChild(listaA);
+		
+		lista.appendChild(listaLi);
+		//listaA.setAttribute("value","Tehtävä "+luku);
+		listaA.textContent="Tehtävä "+luku;
+	}
+}
